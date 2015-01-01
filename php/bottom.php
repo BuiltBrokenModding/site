b711540da8bedfbf9d513d4d5508a143c2acf249
Robert S <rseifert.phone@gmail.com>
2015-01-01 02:06:39 -0500

    Updated copy right at footer

--- php/bottom.php
+++ php/bottom.php
@@ -1,6 +1,8 @@
 </div></div>
 			<div id="footer">
-				<p>&copy;2013-2015 by Built Broken Studios | <a href="about.html">About</a> | <a href="contact.html">Contact</a></p>
+				<p>
+                &copy;2015 by Built Broken Studios | <a href="about.html">About</a> | <a href="contact.html">Contact</a>
+                </p>
 			</div>
 	</body>
 </html>
