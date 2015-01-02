70cb7629a922d97e770284b0da7595a59f19127a
Robert S <rseifert.phone@gmail.com>
2015-01-02 03:14:49 -0500

    Added custom 404 page

---
+++ 404page.php
@@ -0,0 +1,16 @@
+<?php 
+    $page_title="Home";
+    include("php/top.php");
+    
+?>
+    <div>
+        <ul class="listmenu">
+            <li><a onclick="goBack()">Back</a></li>
+            <li><a href="/index.php" >Home</a></li>
+            <li><a href="/project.php" >Projects</a></li>
+        </ul>
+    </div>
+    <h2>404 Page not found</h2>
+    <p> Looks like the page you were looking for does not exist. We recommend you check your spelling and retry. If the link is correct and you reached here from a button then the page has not been created yet. Don't worry about this as the site is still under construction.  </p>
+<?php include("php/bottom.php"); ?>
+				
