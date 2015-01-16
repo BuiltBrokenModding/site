f250f8f6f8a3c041e634fe1154a98d45b900afe6
Robert S <rseifert.phone@gmail.com>
2015-01-16 13:28:05 -0500

    Added google analytics

--- php/bottom.php
+++ php/bottom.php
@@ -8,5 +8,14 @@
                 &copy;2015 by Built Broken Studios | <a href="/about.php">About</a> | <a href="/contact.php">Contact</a>
                 </p>
 			</div>
+            <script>
+              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+              })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+              ga('create', 'UA-58617158-1', 'auto');
+              ga('send', 'pageview');
+            </script>
 	</body>
 </html>
