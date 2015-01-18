b791ca99c0c4f3abeae71785047ec07153a744c0
Robert S <rseifert.phone@gmail.com>
2015-01-18 17:39:42 -0500

    Added some index pages for modpacks

---
+++ pages/modpack/voltz/index.php
@@ -0,0 +1,14 @@
+<?php 
+    $page_title="Voltz";
+    $title ="Voltz Modpack";
+    $sub_title = "PvP Tech themed modpack";
+    $menu = array(
+              'back'    => array('text'=>'Back',  'url'=>'#back')                          
+            );
+    include($_SERVER['DOCUMENT_ROOT'] . "/php/top.php");
+    
+?>
+    <h1>About</h1>
+    <p>TODO add info</p>
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/php/bottom.php"); ?>
+				
