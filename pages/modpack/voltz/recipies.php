fe4ad8589af15c2d08a11342e7efbc33d9867e35
dmodoomsirius <dmodoomsirius@gmail.com>
2015-02-09 19:48:11 -0500

    Added recipies.php where we will post the recipe changes

---
+++ pages/modpack/voltz/recipies.php
@@ -0,0 +1,16 @@
+<?php 
+    $page_title="Voltz";
+    $title ="Voltz Modpack";
+    $sub_title = "PvP Tech themed modpack";
+    $menu = array(
+              'back'    => array('text'=>'Back',  'url'=>'#back')                          
+            );
+    include($_SERVER['DOCUMENT_ROOT'] . "/php/top.php");
+    
+?>
+    <h1>Recipies</h1>
+    <p>We changed the recipies here is what they were changed to.</p>
+    
+    <p><a href="http://technicpack.net/modpack/voltz.552677" target=_blank><img src="https://yampst.net/graphic.php/i.png?s=upload.sign:background.png&icon=upload:icon.png&c1=FFFFFF&c2=FFFFFF&c3=FFFFFF&desc=Thanks%20to%20YAMPST%2C%20I%20keep%20track%20of%20statistics%20for%20my%20mod%20pack!&trackid=ff630939b24be1c330b26f46a178e2378ed0156ec222f488d4a689d3"></img></a>
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/php/bottom.php"); ?>
+				
