947adb34898758f4b30ee5e6b6b4e80bb1f332b1
dmodoomsirius <dmodoomsirius@gmail.com>
2015-02-09 19:47:38 -0500

    Added the tracker banner that is a clickable link to the voltz technic page.

--- pages/modpack/voltz/index.php
+++ pages/modpack/voltz/index.php
@@ -10,5 +10,7 @@
 ?>
     <h1>About</h1>
     <p>TODO add info</p>
+    
+    <p><a href="http://technicpack.net/modpack/voltz.552677" target=_blank><img src="https://yampst.net/graphic.php/i.png?s=upload.sign:background.png&icon=upload:icon.png&c1=FFFFFF&c2=FFFFFF&c3=FFFFFF&desc=Thanks%20to%20YAMPST%2C%20I%20keep%20track%20of%20statistics%20for%20my%20mod%20pack!&trackid=ff630939b24be1c330b26f46a178e2378ed0156ec222f488d4a689d3"></img></a>
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/php/bottom.php"); ?>
 				
