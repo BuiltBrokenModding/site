41c415f67c97730652c6bc0d63506904b59d82d0
Henry Stabell <hennamann9999@gmail.com>
2015-07-01 23:15:38 +0200

    Create concretestairs.php

---
+++ pages/militarybasedecor/wiki/content/blocks/concretestairs.php
@@ -0,0 +1,13 @@
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/militarybasecor/wiki/wiki.top.php"); ?>    
+<h2> Concrete Stairs </h2>
+<p> Concrete Stairs are as the title suggest staircased made using the different tiers of concrete. Each module's concrete block has a stair version (except limecrete) for a list of concrete tiers, check the <a href="concrete.php">Concrete page</a>. The stairs can be colored just like the normal concrete blocks.</p>
+
+</br>
+</br>
+</br>
+<h2>History</h2>
+<p>Has not been released into any version yet</p>
+</br>
+
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/militarybasedecor/wiki/content.php"); ?>
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/php/bottom.php"); ?>
