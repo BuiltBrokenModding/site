f63cf237ef44b540bb45c39b35636ed6677b2a58
Henry Stabell <hennamann9999@gmail.com>
2015-07-01 23:09:04 +0200

    Create concrete.php

---
+++ pages/militarybasedecor/wiki/content/blocks/concrete.php
@@ -0,0 +1,34 @@
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/militarybasecor/wiki/wiki.top.php"); ?>    
+<h2> Concrete </h2>
+<p> Concrete is a base material used for protecting bases but is also used to create a lot of the content in the different modules, There is a different tier of concrete for each module in the mod. </p>
+
+</br>
+</br>
+</br>
+<h2> Concrete Tiers </h2>
+<p> There is one type of concrete for every module, depending on the module the concrete will be weaker or stronger.</p>
+</br>
+<h3> Limecrete <h3>
+<p> Limecrete is the weakest concrete type, it can be crafted with a normal crafting table and is near pointless to make, except for design of course. It cannot be recolored unlike all the other concrete versions. It's added by the gunpowder module.<p>
+</br>
+<h3> Basic Concrete </h3>
+<p> Basic Concrete is a big step up from limecrete but still weaker than the other tiers and also weaker than obsidian but cheaper of course. Unlike limecrete it can be colored. It's added by the vanilla module. </p>
+</br>
+<h3> Reinforced Concrete </h3>
+<p> Reinforced Concrete is as the title suggest basic concrete reinforced with rebar, It is equal in strenght to obsidian but is easier to obtain, especially early in game. Like most of the concrete tiers it can also be colored. It's added by the World War II Module  </p>
+</br> 
+<h3> High Strength Concrete </h3>
+<p> High Strength Concrete is tougher than obsidian but can still be destroyed by the high tier missilies added by ICBM. It can like most other concrete tiers be colored. It's added by the cold war module </p>
+</br>
+<h3> Ultra-High-Performance Concrete </h3>
+<p> Ultra-High-Performance Concrete is the creme-dela-creme of concrete, It can withstand any explosive with the exception of bedrock breaking ones. It can like most other concrete types be colored. It's added by the Future Module. </p>
+
+</br>
+</br>
+</br>
+<h2>History</h2>
+<p>Has not been released into any version yet</p>
+</br>
+
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/militarybasedecor/wiki/content.php"); ?>
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/php/bottom.php"); ?>
