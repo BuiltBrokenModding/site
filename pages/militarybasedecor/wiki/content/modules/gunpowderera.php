144e2e0a9804ee0b34e3f7d769d6577e467f219c
Henry Stabell <hennamann9999@gmail.com>
2015-07-01 22:38:34 +0200

    Create gunpowderera.php

---
+++ pages/militarybasedecor/wiki/content/modules/gunpowderera.php
@@ -0,0 +1,13 @@
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/militarybasecor/wiki/wiki.top.php"); ?>    
+<h2> Gunpowder Era Module </h2>
+<p> The gunpowder era module aims to add content for creating homemade looking bases, based on equipment from the different civil wars, for example: The American Civil War, Spanish Civil War and the Revolutionary War. This module is not needed for any other module to work, and is only recommened if the content sounds interesting to you and your plans! </p>
+
+</br>
+</br>
+</br>
+<h2>History</h2>
+<p>Has not been released into any version yet</p>
+</br>
+
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/militarybasedecor/wiki/content.php"); ?>
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/php/bottom.php"); ?>
