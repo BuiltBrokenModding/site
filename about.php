bf4f8d7725bf351873002f83e73ebbf7ac0fc456
Robert Seifert <rseifert.phone@gmail.com>
2015-07-14 00:29:24 -0400

    Updated about page, Added Kolatra to team page, Added modding team section

--- about.php
+++ about.php
@@ -7,7 +7,8 @@
     
     <p>Built Broken Studios is a small software and game studios based in Ohio, USA. We develop several addons, plugins, and a few small <a href="project.php">projects</a>. Our main focus is making software to support or add to existing games.</p>
     </br>
-    <h2> The Team </h2>    
+    <h2> The Team </h2>
+		<p> Bellow are people who work on our main development team for all projects </p>
         <div class="employees">
             <div>
                 <p>
@@ -44,6 +45,13 @@
                   <a href="https://twitter.com/dmodoomsirius" class="twitter-follow-button" data-align="center" data-show-count="true" data-show-screen-name="false">Follow</a> 
                 </p>
             </div>
+        </div>        
+        <div style="clear:both;"> </div>
+        </br>
+        </br> 
+		<h2> Modding Team </h2> 
+		<p> Bellow are people who work with our modding team for games like minecraft </p>
+        <div class="employees">            
             <div>
                 <p>Hennamann,<br>
                 Developer<br>
@@ -53,30 +61,36 @@
                   <a href="https://twitter.com/Hennamann1" class="twitter-follow-button" data-align="center" data-show-count="true" data-show-screen-name="false">Follow</a> 
                 </p>
             </div>
-            <div>
-                <p>Snow,<br>
+			<div>
+                <p>Kolatra,<br>
                 Developer<br>
-                Email: Unavailable
                 </p>
                 <p>
-                  <a href="https://twitter.com/The_Kinota1" class="twitter-follow-button" data-align="center" data-show-count="true" data-show-screen-name="false">Follow</a> 
+                  <a href="https://twitter.com/ItsKolatra" class="twitter-follow-button" data-align="center" data-show-count="true" data-show-screen-name="false">Follow</a> 
                 </p>
             </div>
         </div>        
         <div style="clear:both;"> </div>
         </br>
-        </br>       
+        </br>		
     <h2> Credits </h2>
         <div class="employees">
             <div>
                 <p>
                     Morton,<br>
                     Asset Developer<br>
-                    Email: Unavailable
                 </p>
                 <p>
                     <a href="https://twitter.com/Morton00000" class="twitter-follow-button" data-align="center" data-show-count="true" data-show-screen-name="false">Follow</a> 
                 </p>                
+            </div>
+			<div>
+                <p>Snow,<br>
+                Developer<br>
+                </p>
+                <p>
+                  <a href="https://twitter.com/The_Kinota1" class="twitter-follow-button" data-align="center" data-show-count="true" data-show-screen-name="false">Follow</a> 
+                </p>
             </div>
         <div style="clear:both;"> </div>
         </br>
