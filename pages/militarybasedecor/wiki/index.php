a0a11c4e2b9b6d9da12c324a4355de565978f185
Henry Stabell <hennamann9999@gmail.com>
2015-09-30 21:38:34 +0200

    Fixed an incorrect link in the mbd wiki

--- pages/militarybasedecor/wiki/index.php
+++ pages/militarybasedecor/wiki/index.php
@@ -3,5 +3,5 @@
     <p>Making missiles and destroying your enemies with them is cool, but it's even more fun with awesome looking military bases. And that's the goal with Military Base Decor; Giving you the resources to create great military bases from any time era!</p>
     </br>
     
-<?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/icbm/wiki/content.php"); ?>
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/militarybasedecor/wiki/content.php"); ?>
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/php/bottom.php"); ?>
