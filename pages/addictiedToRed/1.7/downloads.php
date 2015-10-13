c001a419e99cd5a25e27784917f627e93500f9e0
dmodoomsirius <dmodoomsirius@gmail.com>
2015-10-13 01:15:49 -0400

    update broken download link

--- pages/addictiedToRed/1.7/downloads.php
+++ pages/addictiedToRed/1.7/downloads.php
@@ -11,18 +11,16 @@
     <h2>!!Info!!</h2>
     <p>By downloading these files you agree to the <a href="https://raw.githubusercontent.com/Universal-Electricity/ICBM/master/license.md">EULA</a> </p>
     <h2>!!Alpha Downloads!!</h2>
-    <div id="spoiler" style="display:none">
+    
+     <div id="spoiler" style="display:none">
     <?php 
-        print(file_get_contents("http://$_SERVER[SERVER_NAME]//downloads/dev/AddictedToRed/downloads-1.7.10.php"));
+        print(file_get_contents("http://$_SERVER[SERVER_NAME]//downloads/dev/AddictedToReddownloads-1.7.10.php"));
     ?>
     </div>
     <button title="Click to show/hide content" type="button" onclick="if(document.getElementById('spoiler') .style.display=='none') {document.getElementById('spoiler') .style.display=''}else{document.getElementById('spoiler') .style.display='none'}">Show/hide</button>
     </br>
     </br>
-     <p>At any time if the download plugin fails you can access the downloads threw our <a href="http://adf.ly/2380428/ci.builtbroken.com/maven/">maven</a> file system.</p>
-    </br>
-    </br>
-    <p>At any time if the download plugin fails you can access the downloads threw our <a href="http://adf.ly/2380428/ci.builtbroken.com/maven/">maven</a> file system.</p>
+    <p>At any time if the download plugin fails you can access the downloads threw our <a href="http://adf.ly/2380428/api.dmodoomsirius.me/">maven</a> file system.</p>
     
 <?php 
 include($_SERVER['DOCUMENT_ROOT'] . "/php/bottom.php");
