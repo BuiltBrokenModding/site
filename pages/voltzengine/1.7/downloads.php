29ac9081e8bdb0521e4d801754b587365370c358
dmodoomsirius <dmodoomsirius@gmail.com>
2015-10-13 01:12:40 -0400

    update to the new spoiler setup

--- pages/voltzengine/1.7/downloads.php
+++ pages/voltzengine/1.7/downloads.php
@@ -8,7 +8,12 @@
     <h2>!!Info!!</h2>
     <p>By downloading these files you agree to the <a href="https://raw.githubusercontent.com/Universal-Electricity/ICBM/master/license.md">EULA</a> </p>
     <h2>!!Alpha Downloads!!</h2>
-	<iframe width="80%" height="100%" frameborder="1" src="/downloads/dev/VoltzEngine/downloads-1.7.10.php"></iframe> 
+	<div id="spoiler" style="display:none">
+    <?php 
+        print(file_get_contents("http://$_SERVER[SERVER_NAME]//downloads/dev/VoltzEngine/downloads-1.7.10.php"));
+    ?>
+    </div>
+    <button title="Click to show/hide content" type="button" onclick="if(document.getElementById('spoiler') .style.display=='none') {document.getElementById('spoiler') .style.display=''}else{document.getElementById('spoiler') .style.display='none'}">Show/hide</button>
     </br>
     </br>
      <p>At any time if the download plugin fails you can access the downloads threw our <a href="http://adf.ly/2380428/ci.builtbroken.com/maven/">maven</a> file system.</p>
