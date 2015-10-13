90e76c4a0493b2e89424ef951d373b7b6df58d92
dmodoomsirius <dmodoomsirius@gmail.com>
2015-10-13 01:37:11 -0400

    move show/hide button to top thanks to @zmaster587

--- pages/voltzengine/1.7/downloads.php
+++ pages/voltzengine/1.7/downloads.php
@@ -9,12 +9,12 @@
     <p>By downloading these files you agree to the <a href="https://raw.githubusercontent.com/Universal-Electricity/ICBM/master/license.md">EULA</a> </p>
     <h2>!!Alpha Downloads!!</h2>
     
+    <button title="Click to show/hide content" type="button" onclick="if(document.getElementById('spoiler') .style.display=='none') {document.getElementById('spoiler') .style.display=''}else{document.getElementById('spoiler') .style.display='none'}">Show/hide</button>
 	 <div id="spoiler" style="display:none">
     <?php 
         print(file_get_contents("http://$_SERVER[SERVER_NAME]//downloads/dev/VoltzEngine/downloads-1.7.10.php"));
     ?>
     </div>
-    <button title="Click to show/hide content" type="button" onclick="if(document.getElementById('spoiler') .style.display=='none') {document.getElementById('spoiler') .style.display=''}else{document.getElementById('spoiler') .style.display='none'}">Show/hide</button>
     </br>
     </br>
     <p>At any time if the download plugin fails you can access the downloads through our <a href="http://adf.ly/2380428/api.dmodoomsirius.me/">maven</a> file system.</p>
