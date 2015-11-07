ba8ad7104dd634af6a2f913dfe9c68bf1b889f60
zmaster587 <zmasterfun@gmail.com>
2015-11-06 20:48:15 -0500

    Update index.php
    fix spoilers not working right (tentatively)

--- pages/irc/icbm/index.php
+++ pages/irc/icbm/index.php
@@ -13,8 +13,8 @@
       <li>Tea</li>
       <li>Milk</li>
     </ol>
-    <button title="Click to show/hide content" type="button" onclick="if(document.getElementById('spoiler') .style.display=='none') {document.getElementById('spoiler') .style.display=''}else{document.getElementById('spoiler') .style.display='none'}">Channel Rules</button>
-    <div id="spoiler" style="display:none">
+    <button title="Click to show/hide content" type="button" onclick="if(document.getElementById('spoiler1') .style.display=='none') {document.getElementById('spoiler1') .style.display=''}else{document.getElementById('spoiler1') .style.display='none'}">Channel Rules</button>
+    <div id="spoiler1" style="display:none">
     <p><h2>Channel Rules</h2></p>
     <ol type="1">
       <li>Coffee</li>
@@ -24,8 +24,8 @@
     </div>
     </br>
     </br>
-    <button title="Click to show/hide content" type="button" onclick="if(document.getElementById('spoiler') .style.display=='none') {document.getElementById('spoiler') .style.display=''}else{document.getElementById('spoiler') .style.display='none'}">Click here to show channel name.</button>
-    <div id="spoiler" style="display:none">
+    <button title="Click to show/hide content" type="button" onclick="if(document.getElementById('spoiler2') .style.display=='none') {document.getElementById('spoiler2') .style.display=''}else{document.getElementById('spoiler2') .style.display='none'}">Click here to show channel name.</button>
+    <div id="spoiler2" style="display:none">
     <p>ICBM</p>
     <p> This irc channel is located on irc.esper.net. It is mainly used for our ICBM minecraft mod.</p>
     <script async defer data-hostname="irc.esper.net" data-channel="#icbm" data-port="6667" src="https://www.irccloud.com/static/invite.js"></script></script>
