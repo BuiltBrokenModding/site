dd221e1f1249dd5738db754a1376380f45103ed1
sfxworks <sfxworks@gmail.com>
2015-12-11 11:27:10 -0500

    enderblocks
    grammar error

--- pages/icbm/wiki/content/explosives/enderblocks.php
+++ pages/icbm/wiki/content/explosives/enderblocks.php
@@ -6,7 +6,7 @@
     <iframe align="right" width="420" height="315" src="https://www.youtube.com/embed/87vlED5D-_Y?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0"></iframe>
     </div>
     <div style="float:left; width:50%">
-    <p>A Micro Warhead that harnesses the power of the don't-get-to-know-too-well enderman's teleporting ability. This missile will teleport blocks to a random location rather than destroying them. Great for "misplacing" a friend's chest.?</p>
+    <p>A Micro Warhead that harnesses the power of the don't-get-to-know-too-well enderman's teleporting ability. This missile will teleport blocks to a random location rather than destroying them. Great for "misplacing" a friend's chest.</p>
     <br/>
     <h2 style="text-align:center">Recipe</h2>
     <img style="display:block; margin-left:auto; margin-right:auto" src="../../../../../img/icbm/recipe-microwarhead-ender-blocks.png" width="108" height="108" alt="Micro Warhead - Ender Blocks Fit recipe" width="108" height="108" alt="Micro Warhead - Endothermic Fit recipe"/>
