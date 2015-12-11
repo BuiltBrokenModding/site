ace405b7fea4fa143af74ac2d6aba18079841eb3
sfxworks <sfxworks@gmail.com>
2015-12-11 08:55:14 -0500

    Added Flash Fire
    Completed flash-fire.php

--- pages/icbm/wiki/content/explosives/flash-fire.php
+++ pages/icbm/wiki/content/explosives/flash-fire.php
@@ -1,18 +1,26 @@
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/icbm/wiki/wiki.top.php"); ?>
-    <h2> Video </h2>
-    iframe
-    <h2>Name</h2>
-    <p>description</p>
+    <h2 style="text-align:center"> Micro Warhead - Flash Fire Fit </h2>
     <br/>
-    <h2>Recipe</h2>
-    image
+    <div>
+    <div style="float:left; width:50%">
+    <iframe width=100% height=315px src="https://www.youtube.com/embed/X-MrPbv3lsg?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+    </div>
+    <div style="float:left; width:50%">
+      <p>The odds of a flash flood coming in to save your friends statue of himself burning is pretty low. The Flash Fire Fit, with a radius of two, creates fire at your desired location. Launch with a Missile Launcher for direct hits.</p>
+      <br/>
+    <h2 style="text-align:center">Recipe</h2>
+    <img style="display:block; margin-left:auto; margin-right:auto" src="../../../../../img/icbm/recipe-microwarhead-flash-fire.png" width="108" height="108" alt="Micro Warhead - Flash Fire Fit recipe"/>
     <br/>
-     description
+    <p>
+    Grab that blaze rod you were going to burn your friend's house down with. Fit it onto the Micro Warhead instead for a little more fun.
+    </p>
+    <br/>
     <br/>
     <h2>History</h2>
     <p>#2.3.0 - Added in the new 1.7.10 release</p>
-    </br>
+    <br/>
+    </div>
+    </div>
     
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/icbm/wiki/content.php"); ?>
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/php/bottom.php"); ?>
-				
