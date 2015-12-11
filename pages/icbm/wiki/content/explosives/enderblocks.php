c4eff7e06512037bb58f0ad795bb49510092d1df
sfxworks <sfxworks@gmail.com>
2015-12-11 10:23:08 -0500

    Applied styles to enderblocks.php
    Applied styles to enderblocks.php

--- pages/icbm/wiki/content/explosives/enderblocks.php
+++ pages/icbm/wiki/content/explosives/enderblocks.php
@@ -1,18 +1,26 @@
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/icbm/wiki/wiki.top.php"); ?>
-    <h2> Micro Warhead - Ender Blocks Fit</h2>
-    <iframe width="420" height="315" src="https://www.youtube.com/embed/87vlED5D-_Y?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
-    <h2>Ender Blocks Fit</h2>
-    <p>A microwarhead that harnesses the power of the don't-get-to-know-too-well enderman's teleporting ability. This missile will teleport blocks to a random location rather than destroying them. Great for "misplacing" a friend's chest.</p>
+    <h2 style="text-align:center"> Micro Warhead - Endothermic Fit </h2>
     <br/>
-    <h2>Recipe</h2>
-    <img src="../../../../../img/icbm/recipe-microwarhead-ender-blocks.png" width="108" height="108" alt="Micro Warhead - Ender Blocks Fit recipe"/>
+    <div>
+    <div style="float:left; width:47%; margin-right:3%">
+    <iframe align="right" width="420" height="315" src="https://www.youtube.com/embed/87vlED5D-_Y?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0"></iframe>
+    </div>
+    <div style="float:left; width:50%">
+    <p>A microwarhead that harnesses the power of the don't-get-to-know-too-well enderman's teleporting ability. This missile will teleport blocks to a random location rather than destroying them. Great for "misplacing" a friend's chest.?</p>
     <br/>
+    <h2 style="text-align:center">Recipe</h2>
+    <img style="display:block; margin-left:auto; margin-right:auto" src="../../../../../img/icbm/recipe-microwarhead-ender-blocks.png" width="108" height="108" alt="Micro Warhead - Ender Blocks Fit recipe" width="108" height="108" alt="Micro Warhead - Endothermic Fit recipe"/>
+    <br/>
+    <p>
     Craft this Micro Warhead with a Gold Nugget, an Ender Pearl, and a Micro Warhead.
+    </p>
     <br/>
     <br/>
     <h2>History</h2>
     <p>#2.3.0 - Added in the new 1.7.10 release</p>
-    </br>
+    <br/>
+    </div>
+    </div>
     
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/icbm/wiki/content.php"); ?>
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/php/bottom.php"); ?>
