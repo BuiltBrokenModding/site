5f1c5254926e24fc4151dc14dd058610be6e9e8e
sfxworks <sfxworks@gmail.com>
2015-12-11 05:08:12 -0500

    Templates & Fixes
    Added page templates. Fixed grammar & spelling on tnt.php

---
+++ pages/icbm/wiki/content/explosives/exothermic.php
@@ -0,0 +1,17 @@
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/icbm/wiki/wiki.top.php"); ?>
+    <h2> Video </h2>
+    iframe
+    <h2>Name</h2>
+    <p>description</p>
+    <br/>
+    <h2>Recipe</h2>
+    image
+    <br/>
+     description
+    <h2>History</h2>
+    <p>#2.3.0 - Added in the new 1.7.10 release</p>
+    </br>
+    
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/icbm/wiki/content.php"); ?>
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/php/bottom.php"); ?>
+				
--- pages/icbm/wiki/content/explosives/tnt.php
+++ pages/icbm/wiki/content/explosives/tnt.php
@@ -2,10 +2,10 @@
      <h2> Micro Warhead - TNT Fit </h2>
         <iframe width="480" height="360" src="https://www.youtube.com/embed/nk9wEPmewEA?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
     <h2>TNT Explosive</h2>
-    <p>A micro warhead is fitted with a TNT Explosive. Minor explision (2 block radius). It is not recommended to have a Misile Launder and carefully aim it at a conveniently placed bundle of TNT that may be convinently be next to your friend's base.   </p>
+    <p>A micro warhead is fitted with a TNT Explosive. Minor explosion (2 block radius). It is not recommended to have a Missile Launder that is carefully aimed at a conveniently placed bundle of TNT. This applies to one that may be conveniently be next to your friend's base.</p>
     <br/>
     <h2>Recipe</h2>
-    <img src="../../../../../img/icbm/recipe-microwarhead-tnt.png" width="108" height="109" alt="Micro Warhead - TNT Fit Recepie"/>
+    <img src="../../../../../img/icbm/recipe-microwarhead-tnt.png" width="108" height="109" alt="Micro Warhead - TNT Fit Recipe"/>
     <br/>
      Simply fit two gunpowder on top of a Micro Warhead.
     <br/>
@@ -15,4 +15,3 @@
     
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/icbm/wiki/content.php"); ?>
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/php/bottom.php"); ?>
-				
