731c92c2c1854a71247ab0ccb08f2b6e25c578e4
sfxworks <sfxworks@gmail.com>
2015-12-11 09:29:49 -0500

    let-it-snow.php
    Finished let-it-snow.php content

--- pages/icbm/wiki/content/explosives/let-it-snow.php
+++ pages/icbm/wiki/content/explosives/let-it-snow.php
@@ -1,18 +1,28 @@
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/icbm/wiki/wiki.top.php"); ?>
-    <h2> Video </h2>
-    iframe
-    <h2>Name</h2>
-    <p>description</p>
+    <h2 style="text-align:center"> Micro Warhead - Let It Snow Fit </h2>
     <br/>
-    <h2>Recipe</h2>
-    image
+    <div>
+    <div style="float:left; width:50%">
+    <iframe width=100% height=315px src="https://www.youtube.com/embed/SfYcF8fqjl0?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+    </div>
+    <div style="float:left; width:50%">
+    <p>Did you ever want to build a snowman? Well now you can come on and play with the Micro Warhead Let It Snow Fit! Launch a missile that spawns two Snow Golems. Warning: Keep away from <a href="flash-fire.php">Flash-Fire</a>, <a href="exothermix.php">Exothermic</a>, and <a href="fire-bomb.php">Fire-Bomb</a> type warheads. Otherwise, you'll never see them anymore.</p>
     <br/>
-     description
+    <!--
+    <h2 style="text-align:center">Recipe</h2>
+    <img style="display:block; margin-left:auto; margin-right:auto" src="../../../../../img/icbm/recipe-microwarhead-let-it-snow.png" width="108" height="108" alt="Micro Warhead - Let It Snow Fit recipe"/>
+    <br/>
+    <p>
+    Two Ice Blocks and one Micro Warhead will bring a chill to your inventory in seconds.
+    </p>
+    -->
+    <br/>
     <br/>
     <h2>History</h2>
-    <p>#2.3.0 - Added in the new 1.7.10 release</p>
-    </br>
+    <p>Previewed on 12/10/2015 on <a href="https://www.youtube.com/watch?v=SfYcF8fqjl0">DarkGuardsman's channel</a>.</p>
+    <br/>
+    </div>
+    </div>
     
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/icbm/wiki/content.php"); ?>
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/php/bottom.php"); ?>
-				
