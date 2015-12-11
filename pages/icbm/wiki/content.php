2628e64c6ab39dfd54ceed16858c56189db2a011
Robert Seifert <rseifert.phone@gmail.com>
2015-12-11 06:29:53 -0500

    Setup content pane for all explosives

--- pages/icbm/wiki/content.php
+++ pages/icbm/wiki/content.php
@@ -13,7 +13,16 @@
                 'silo' => array('text'=>'Silo',  'url'=> $web_root . 'machines/silo.php')                 
             );
     $explosives = array(
-                'tnt' => array('text'=>'TNT',  'url'=> $web_root . 'explosives/tnt.php')     
+                'antimatter' => array('text'=>'Antimatter',  'url'=> $web_root . 'explosives/antimatter.php'),
+                'arrow' => array('text'=>'Arrow',  'url'=> $web_root . 'explosives/arrow.php'),
+                'enderblocks' => array('text'=>'Ender Blocks',  'url'=> $web_root . 'explosives/enderblocks.php'),
+                'endothermic' => array('text'=>'Endothermic',  'url'=> $web_root . 'explosives/endothermic.php'),
+                'exothermic' => array('text'=>'Exothermic',  'url'=> $web_root . 'explosives/exothermic.php'),
+                'fireBomb' => array('text'=>'Fire Bomb',  'url'=> $web_root . 'explosives/fire-bomb.php'),
+                'flashFire' => array('text'=>'Flash Fire',  'url'=> $web_root . 'explosives/flash-fire.php'),
+                'letItSnow' => array('text'=>'Let it Snow',  'url'=> $web_root . 'explosives/let-it-snow.php'),
+                'tnt' => array('text'=>'TNT',  'url'=> $web_root . 'explosives/tnt.php'),
+                'torch-eater' => array('text'=>'Torch Eater',  'url'=> $web_root . 'explosives/torch-eater.php')                
             );
             
 ?>
