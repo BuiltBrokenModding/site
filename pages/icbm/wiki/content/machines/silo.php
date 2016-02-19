2f66d6d58e07220c2cfd35352ece439654924af6
Robert Seifert <rseifert.phone@gmail.com>
2016-02-19 12:19:08 -0500

    Updated wiki header, Updated silo page with generic info

--- pages/icbm/wiki/content/machines/silo.php
+++ pages/icbm/wiki/content/machines/silo.php
@@ -1,12 +1,15 @@
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/icbm/wiki/wiki.top.php"); ?>    
 <h2> Silo </h2>
-<p> Large version of a launcher that can only face up. Can support <a href="/pages/icbm/wiki/content/missiles/medium.php">medium</a> and <a href="/pages/icbm/wiki/content/missiles/large.php">large</a> missiles though each needs a different size silo. </p>
+<p> The silo is designed to be a static missile launching system. Built in several different sizes supporting each unique <a href="https://en.wikipedia.org/wiki/Missile">missile</a> set. Each silo has slighltly different requirements for missiles, launching systems, and setup. All silos require a <a href="/pages/icbm/wiki/content/machines/controller.php">controller</a> in order to fire missiles.</p>
+<h2> Small Silo </h2>
+<p> Small silo supports firing a single <a href="/pages/icbm/wiki/content/missiles/small.php">small missile</a> and is 3 blocks tall. It requires a solid surface to be placed and has no GUI. Missile can be inserted by right clicking and removed the same way.</p>
+<img src="../../../../../img/icbm/smallMissile/launcher.png">
 
 </br>
 </br>
 </br>
 <h2>History</h2>
-<p>Has not been released into any version yet</p>
+<p>#2.3.0 - Added Small silo</p>
 </br>
 
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/icbm/wiki/content.php"); ?>
--- pages/icbm/wiki/wiki.top.php
+++ pages/icbm/wiki/wiki.top.php
@@ -5,5 +5,6 @@
     include($_SERVER['DOCUMENT_ROOT'] . "/php/top.wiki.php");
    
 ?>   
-<p style="padding:0px;"> <strong>Note:</strong> The wiki is still a work in progress so will not contain all information about the mod</p>
+<p style="padding:0px;"> <strong>Note:</strong> Wiki is not updated often, content may be outdated.</p>
+<p style="padding:0px;"> <strong>Note:</strong> For up to date recipes use mods like <a href="http://chickenbones.net/Pages/links.html">NEI</a> or <a href="http://www.curse.com/mc-mods/minecraft/238222-just-enough-items-jei">JEI</a> for recipes</p>
 </br>
