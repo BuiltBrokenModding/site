099582f7f130e6345859f3f59b2e137272fa5fd9
Robert Seifert <rseifert.phone@gmail.com>
2016-03-14 06:10:25 -0400

    Filled in basic info for data chip, gps data chip, and controller

--- pages/icbm/wiki/content/tools/datachip.php
+++ pages/icbm/wiki/content/tools/datachip.php
@@ -1,6 +1,14 @@
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/icbm/wiki/wiki.top.php"); ?>    
-<h2> Title of page </h2>
-<p> Body of the page </p>
+<h2> Data Chip </h2>
+<img src="../../../../../img/icbm/datachip.png" alt="Data Chip" style="width:40%;height:40%;">
+<p> The data chip is used to store data from one machine and transfer it to another. </p>
+<h3>Linking</h3>
+<p> The main use of the chip in the mod is to link machines to each other. To do this first shift-right click a machine that you want to create a link from. For example a missile <a href="/pages/icbm/wiki/content/machines/silo.php">silo</a>, chat will update saying link data set.</p>
+<img src="../../../../../img/icbm/datachip.linkset.png" alt="Data Chip Link Set" style="width:40%;height:40%;">
+<p> Its also useful to note the chip will change colors when data is stored. </p>
+<img src="../../../../../img/icbm/datachip.data.png" alt="Data Chip" style="width:10%;height:10%;">
+<p>Next right the machine you want to link to, for example a missile <a href="/pages/icbm/wiki/content/machines/controller.php">controller</a>, chat will update saying link created.</p>
+<img src="../../../../../img/icbm/datachip.linkcreated.png" alt="Data Chip Link Created" style="width:40%;height:40%;">
 
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/icbm/wiki/content.php"); ?>
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/php/bottom.php"); ?>
