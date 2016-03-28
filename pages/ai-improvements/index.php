ba3d505fe5e7682fbd5f4b6654c5d0a1e452edf6
dmodoomsirius <dmodoomsirius@gmail.com>
2016-03-27 23:18:33 -0400

    uncomment modpack nav link

--- pages/ai-improvements/index.php
+++ pages/ai-improvements/index.php
@@ -6,7 +6,7 @@
               'back'    => array('text'=>'Back',  'url'=>'#back'),   
               #'archive'   => array('text'=>'Archive',  'url'=>'archive.php'),
               #'mc1.7'   => array('text'=>'MC1.7',  'url'=>'1.7/'),
-              #'modpacks'   => array('text'=>'Modpacks',  'url'=>'../modpack.php'),
+              'modpacks'   => array('text'=>'Modpacks',  'url'=>'../modpack.php'),
               'curse'   => array('text'=>'Curse',  'url'=>'http://minecraft.curseforge.com/projects/ai-improvements'),
               'code'    => array('text'=>'Src',  'url'=>'https://github.com/BuiltBrokenModding/AI-Improvements'),
 			  #'wiki' => array('text'=>'Wiki', 'url'=>'wiki/')
