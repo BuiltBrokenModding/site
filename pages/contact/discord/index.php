799ba58c934bd763c5fe686802accdcb7f30dc4f
dmodoomsirius <dmodoomsirius@gmail.com>
2016-03-31 07:42:28 -0400

    remove discord info from spoiler

--- pages/contact/discord/index.php
+++ pages/contact/discord/index.php
@@ -5,8 +5,6 @@
 ?>
     <p>Here is some information about our Discord server</p>
     <p>Discord</p>
-    <button title="Click to show/hide content" type="button" onclick="if(document.getElementById('spoilerIC') .style.display=='none') {document.getElementById('spoiler') .style.display=''}else{document.getElementById('spoiler') .style.display='none'}">Show/hide</button>
-    <div id="spoilerIC" style="display:none">
     <iframe src="https://discordapp.com/widget?id=97599288397275136&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe>    </div>
     </br>
     </br>
