c2bab408e02c49415d88b53bcbf6e4281e40e57b
Samuel Walker <sfxworks@gmail.com>
2016-04-10 01:08:21 -0400

    New header and home
    Construct of new header and homepage. Not on main index page.

---
+++ index3.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<meta charset="utf-8">
+<meta name="keywords" content="icbm, builtbroken, minecraft mod, addicted to red, voltz engine, assembly line, armory, artillects, tinker toys, military base detector">
+<meta name="description" content="A collection of minecraft mods and coding projects developed and maintained by the BuiltBroken Modding group.">
+<title>index</title>
+<link href="css/main.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<ul> <!--Home Github Games Projects Modpacks About -->
+	<li><a class="active" href="#home">Home</a></li>
+    <li><a href="https://github.com/BuiltBrokenModding">Github</a></li>
+    <li><a href="/games">Games</a></li>
+    <li><a href="/projects">Projects</a></li>
+    <li style="float:right"><a href="/about">About</a></li>
+</ul>
+</body>
+</html>
