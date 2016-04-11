e53fa68addaa3da49440a7c8ae0937f183aadb76
Samuel Walker <sfxworks@gmail.com>
2016-04-10 23:13:21 -0400

    Update to Index3
    Background, sidebar, and header complete.

--- index3.php
+++ index3.php
@@ -6,15 +6,33 @@
 <meta name="description" content="A collection of minecraft mods and coding projects developed and maintained by the BuiltBroken Modding group.">
 <title>index</title>
 <link href="css/main.css" rel="stylesheet" type="text/css">
+<script>var __adobewebfontsappname__="dreamweaver"</script>
+<script src="http://use.edgefonts.net/vast-shadow:n4:default;pt-sans-narrow:n4:default.js" type="text/javascript"></script>
+
 </head>
 
 <body>
-<ul> <!--Home Github Games Projects Modpacks About -->
-	<li><a class="active" href="#home">Home</a></li>
-    <li><a href="https://github.com/BuiltBrokenModding">Github</a></li>
-    <li><a href="/games">Games</a></li>
-    <li><a href="/projects">Projects</a></li>
-    <li style="float:right"><a href="/about">About</a></li>
-</ul>
+<!-- Header -->
+<div class="top">
+    <h1>Built Broken Studios</h1>
+    <h2>Build it - Break it - Reanimate it</h2>
+</div>
+<div class="nav">
+    <ul> <!--Home Github Games Projects Modpacks About -->
+        <li><a class="active" href="#home">Home</a></li>
+        <li><a href="/about">About</a>	</li>
+        <li><a href="/games">Games</a></li>
+        <li><a href="/projects">Projects</a></li>
+        
+        <li style="float:right"><a href="https://github.com/BuiltBrokenModding">Github</a></li>
+    </ul>
+</div>
+
+<div class="page">
+
+
+</div>
+
+
 </body>
 </html>
