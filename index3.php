a247115e74a2a4a7cee55bcf5775c43c9ee47032
Samuel Walker <sfxworks@gmail.com>
2016-04-12 18:56:09 -0400

    Flashbanner
    Flashbanner on index3

--- index3.php
+++ index3.php
@@ -6,6 +6,7 @@
 <meta name="description" content="A collection of minecraft mods and coding projects developed and maintained by the BuiltBroken Modding group.">
 <title>index</title>
 <link href="css/main.css" rel="stylesheet" type="text/css">
+<script src="js/swfobject_modified.js"></script>
 <script>var __adobewebfontsappname__="dreamweaver"</script>
 <script src="http://use.edgefonts.net/vast-shadow:n4:default;pt-sans-narrow:n4:default.js" type="text/javascript"></script>
 
@@ -17,22 +18,48 @@
     <h1>Built Broken Studios</h1>
     <h2>Build it - Break it - Reanimate it</h2>
 </div>
-<div class="nav">
-    <ul> <!--Home Github Games Projects Modpacks About -->
-        <li><a class="active" href="#home">Home</a></li>
-        <li><a href="/about">About</a>	</li>
-        <li><a href="/games">Games</a></li>
-        <li><a href="/projects">Projects</a></li>
-        
-        <li style="float:right"><a href="https://github.com/BuiltBrokenModding">Github</a></li>
-    </ul>
+<div class="pagecontent">
+    <div class="nav">
+        <ul> <!--Home Github Games Projects Modpacks About -->
+            <li><a class="active" href="#home">Home</a></li>
+            <li><a href="/about">About</a>	</li>
+            <li><a href="/games">Games</a></li>
+            <li><a href="/projects">Projects</a></li>
+            
+            <li style="float:right"><a href="https://github.com/BuiltBrokenModding">Github</a></li>
+        </ul>
+    </div>
+    
+    <div class="banner">
+      <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="800" height="450" id="FlashID" title="BBS FlashBanner">
+        <param name="movie" value="flash/BBM-FlashBanner/src/FlashBanner.swf">
+        <param name="quality" value="high">
+        <param name="wmode" value="opaque">
+        <param name="swfversion" value="6.0.65.0">
+        <!-- This param tag prompts users with Flash Player 6.0 r65 and higher to download the latest version of Flash Player. Delete it if you don’t want users to see the prompt. -->
+        <param name="expressinstall" value="flash/BBM-FlashBanner/src/expressInstall.swf">
+        <!-- Next object tag is for non-IE browsers. So hide it from IE using IECC. -->
+        <!--[if !IE]>-->
+        <object type="application/x-shockwave-flash" data="flash/BBM-FlashBanner/src/FlashBanner.swf" width="800" height="450">
+          <!--<![endif]-->
+          <param name="quality" value="high">
+          <param name="wmode" value="opaque">
+          <param name="swfversion" value="6.0.65.0">
+          <param name="expressinstall" value="flash/BBM-FlashBanner/src/expressInstall.swf">
+          <!-- The browser displays the following alternative content for users with Flash Player 6.0 and older. -->
+          <div>
+            <h4>Content on this page requires a newer version of Adobe Flash Player.</h4>
+            <p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" width="112" height="33" /></a></p>
+          </div>
+          <!--[if !IE]>-->
+        </object>
+        <!--<![endif]-->
+      </object>
+    </div>
+    <script type="text/javascript">
+    swfobject.registerObject("FlashID");
+    </script>
 </div>
-
-<div class="page">
-
-
-</div>
-
 
 </body>
 </html>
