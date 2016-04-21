9363c063b0e45ce85d03743e8a48b4f9c934c1a7
GreenCafe <worldbrodcast@gmail.com>
2016-04-20 20:15:33 -0600

    Added Launcher Wiki - IMG's MIGHT BE BROKEN

--- pages/icbm/wiki/content/machines/launcher.php
+++ pages/icbm/wiki/content/machines/launcher.php
@@ -1,6 +1,9 @@
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/icbm/wiki/wiki.top.php"); ?>    
-<h2> Title of page </h2>
-<p> Body of the page </p>
+<h2> Missile Launcher </h2>
+<p> A Missile Launcher is a 7 block tall structure built to be a launcher and the workstation to build missiles.</p>
+<img src="\img\icbm\launcher\empty_launcher.png" alt="" style="max-width:100%; height:auto;"/>
+<img src="\img\icbm\launcher\loaded_launcher.png" alt="" style="max-width:100%; height:auto;"/>
+<p>It is built from 1 Launcher Computer at the bottom and 6 Launcher Frames above it. You then need to take a screwdrive and Shift+Right-Click the Launcher Computer to make it a complete Missile Launcher.</p>
 
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/icbm/wiki/content.php"); ?>
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/php/bottom.php"); ?>
