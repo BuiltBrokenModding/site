e8d905423b86d00fdb2ac3bb3856aff98b28fed9
Robert Seifert <rseifert.phone@gmail.com>
2016-11-09 14:06:33 -0500

    News 11/9/2016

--- index.php
+++ index.php
@@ -23,6 +23,29 @@
     <hr>
     <div class="news">
                 <div>
+				<h3>The plan</h3>
+				<h4>Date: 9/15/2016</h4>
+				<br>
+                <p> I noted this on Youtube and twitter a while back that I have a new plan. As well I have a new deal for you, the users, that should make you all happy. I will not be leaving modding right away. Yes, I know I said I would be leaving but here is what I mean. I will not leave until A. I find a replacement B. All mods are updated to 1.10 C. Everything is in a nice shape before I leave. As well if you, the users, can keep update downloads & donations I will stick around. Basicly I will consider donations as you, the users, hiring me to keep working on my mods. The more donations I get the longer I will work per week on the mods, up to 50 hours as I do need to sleep & do school work. </p>
+                <p> Outside of this new deal I have a plan for getting all the mods rolling out the door faster. To start with the plan per project list that I posted when I said I would quit is subject to change. This will include Assembly Line which is now set for alpha release this week. It hurts to die will be continued for Voltz only. Assembly line will be continued and progress will start next week. Expected released date is here in 2-3 weeks depending on how long it takes to write. Sentry guns and Tinker Toys are no longer canceled but are on hold until they can be designed. </p>
+                <h4>Work plan</h4>
+                <p> Bellow is my work plan and is subject to change. Dates are the start of the week unless its a release date. </p>
+                <ul>
+                    <li> 11/6 - Rail system </li>
+                    <li> 11/6 - Assembly line revive </li>
+                    <li> 11/11 - ICBM Version Release </li>
+                    <li> 11/11 - Assembly Line Alpha Release</li>
+                    <li> 11/13 - Armory Mod </li>
+                    <li> 11/20 - Armory Mod </li>
+                    <li> 11/27 - Basic Industries </li>
+                    <li> 12/2 - Armory Alpha Release </li>
+                    <li> 12/4 - Basic Industries </li>    
+                    <li> 12/9 - Basic Industries Alpha Release </li>    
+                    <li> 12/11 - Bug Fixing </li>     
+                    <li> 12/18 - Voltz 2.0 Alpha </li>                       
+                </ul>
+                </div>
+                <div>
 				<h3>A new direction</h3>
 				<h4>Date: 9/15/2016</h4>
 				<br>
--- pages/icbm/wiki/content/machines/warheadtable.php
+++ pages/icbm/wiki/content/machines/warheadtable.php
@@ -1,6 +1,6 @@
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/icbm/wiki/wiki.top.php"); ?>    
-<h2> Title of page </h2>
-<p> Body of the page </p>
+<h2> Warhead Workstation </h2>
+<p> The warhead workstation is used to craft and customize the warhead. </p>
 
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/icbm/wiki/content.php"); ?>
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/php/bottom.php"); ?>
