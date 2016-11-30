5071d621fc79c4cd32b459ac655b7c60692dd5b8
quantomworks <sfxworks@gmail.com>
2016-11-30 23:55:21 +0000

    Made banner script dynamic. Reapplied script to index.html

--- js/banner.js
+++ js/banner.js
@@ -1,33 +1,33 @@
-var imageArray = ["bt_1", "cc_1", "icbm_1", "rc_1", "wb_1", "wr_1"];
-var textArray = ["BT", "CC", "ICBM", "RC", "WB", "WR"];
-var photoImg = document.getElementById("photoBannerImage");
-var photoText = document.getElementById("photoText");
+function bannerInit(imgSource, imageArray, textArray, photoImg, photoText)
+{
+	var imgIndex = 0
 
-var imgIndex = 0
+	function fadeOut() {  
+		photoImg.style.opacity -= 0.05
+		photoText.style.opacity -= 0.05
+		
+	  if(photoImg.style.opacity <= 0) {
+		imgIndex = (imgIndex + 1) % imageArray.length
+		photoImg.src = imgSource + imageArray[imgIndex] + ".png"
+		photoText.innerHTML = textArray[imgIndex];
+		setTimeout(fadeIn, 1000)
+	  } else {
+		setTimeout(fadeOut, 12)
+	  }
+	}
 
-function fadeOut() {  
-	photoImg.style.opacity -= 0.05
-	photoText.style.opacity -= 0.05
+	function fadeIn() {
+		photoImg.style.opacity = (+photoImg.style.opacity) + 0.05
+		photoText.style.opacity = (+photoImg.style.opacity) + 0.05
+		
+	  if(photoImg.style.opacity >= 1) {
+		setTimeout(fadeOut, 8000)
+	  } else {
+		setTimeout(fadeIn, 12)
+	  }
+	}
 	
-  if(photoImg.style.opacity <= 0) {
-  	imgIndex = (imgIndex + 1) % imageArray.length
-    photoImg.src = "img/banner/" + imageArray[imgIndex] + "_254.png"
-    photoText.innerHTML = textArray[imgIndex];
-  	setTimeout(fadeIn, 1000)
-  } else {
-  	setTimeout(fadeOut, 12)
-  }
+	fadeOut()
 }
 
-function fadeIn() {
-	photoImg.style.opacity = (+photoImg.style.opacity) + 0.05
-	photoText.style.opacity = (+photoImg.style.opacity) + 0.05
-	
-  if(photoImg.style.opacity >= 1) {
-  	setTimeout(fadeOut, 8000)
-  } else {
-  	setTimeout(fadeIn, 12)
-  }
-}
 
-fadeOut()
