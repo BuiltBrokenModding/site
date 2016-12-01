856af1cc47db55af1d9f1b6ca41ce2a6553df74f
quantomworks <sfxworks@gmail.com>
2016-12-01 02:22:16 +0000

    Applied dynamic banner script to addons.html. Completed scripts for addons.html

---
+++ js/addonData.js
@@ -0,0 +1,8 @@
+var addonDataArray = [
+{
+"image":"icbmimage.png",
+"title":"ICBM",
+"description":"Nukes, Destruction, Chaos. Build and destroy using bla bla bla..." //Short description for icon auto generated and derived from first 3 words of description.
+}
+];
+
