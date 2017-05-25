8fcb85f993b3e6ddbd659bb480856fcf2b630360
Samuel Walker <sfxworks@gmail.com>
2017-05-25 13:47:33 -0400

    Patched Addons
    Patching in addons. Added detail and links to json file. Images
    included.

--- js/addonData.js
+++ js/addonData.js
@@ -1,7 +1,49 @@
 var addonDataArray = [
 {
-"image":"icbmimage.png",
-"title":"ICBM",
-"description":"Nukes, Destruction, Chaos. Build and destroy using bla bla bla..." //Short description for icon auto generated and derived from first 3 words of description.
+	"image":"img/logos/icbm.png",
+	"title":"ICBM",
+	"description":"We all have that guy up the road we just want to nuke. You know the one who keeps firing his TNT cannons thinking he is the king of war. With ICBM not only is he outmatched, you can get that wish you always wanted. " //Short description for icon auto generated and derived from first 3 words of description.
+},
+{
+	"image":"img/logos/addictedtored.png",
+	"title":"Addicted To Red",
+	"description":"When you close your eyes at night, is the only thing you can think of Redstone? Good, you've come to the right place. We too have become addicted to the power of redstone and have made it even more powerful. Giving redstone all those detectors, manipulators, and cool machines it's always needed."
+},
+{
+	"image":"img/logos/aiimprovements.png",
+	"title":"AI Improvements",
+	"description":"If you have ever looked at a CPU usage report for Minecraft you will notice AI code at the top. This mod adds some changes to the AI code to decrease how much CPU the code uses."
+},
+{
+	"image":"img/logos/bonetorch.png",
+	"title":"Bone Torch",
+	"description":"Light the halls with the bones of your enemies!"
+},
+{
+	"image":"img/logos/coloredchests.png",
+	"title":"Colored Chests",
+	"description":"Always hate it how you can't place double chests next to each other or figure out what chest is which. Colored chests adds a large variety of chests that support any RGB value."
+},
+{
+	"image":"img/logos/redcow.png",
+	"title":"Red Cow",
+	"description":"Parody of Redbull energy drinks. This mod adds a potion item in the shape of a soda can that lets you fly. It even gives you wings on your back for the lolz of it."
+},
+{
+	"image":"img/logos/universalelectricity.png",
+	"title":"Universal Electricity",
+	"description":"In the early game, it can be a pain to get a bucket for farming. This mod adds several wooden bucket types for storing any fluid without adding a new item id per fluid."
+},
+{
+	"image":"img/logos/woodenbuckets.png",
+	"title":"Wooden Buckets",
+	"description":"In the early game, it can be a pain to get a bucket for farming. This mod adds several wooden bucket types for storing any fluid without adding a new item id per fluid."
+},
+{
+	"image":"img/logos/woodenrails.png",
+	"title":"Wooden Rails",
+	"description":"In the early game, it can be tricky getting into using minecarts. Especially with the high iron cost and all your machines needing that iron first. Wooden rails fixes this by adding wooden carts and tracks that are just a little slower than iron versions. Allowing for intergration of rail systems into your base without the pain of mining a lot of iron."
 }
+
+
 ];
