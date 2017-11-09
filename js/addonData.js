fe6b5b5bbaf183fbf6c400ae4b51fecf6f4026f7
Samuel Walker <sfxworks@gmail.com>
2017-11-09 03:46:00 -0500

    Addon Data
    Updated addon data from https://minecraft.curseforge.com/members/DarkCow/projects

--- js/addonData.js
+++ js/addonData.js
@@ -1,49 +1,110 @@
 var addonDataArray = [
 {
-	"image":"img/logos/icbm.png",
-	"title":"ICBM",
-	"description":"We all have that guy up the road we just want to nuke. You know the one who keeps firing his TNT cannons thinking he is the king of war. With ICBM not only is he outmatched, you can get that wish you always wanted. " //Short description for icon auto generated and derived from first 3 words of description.
+	"image":"https://media-elerium.cursecdn.com/avatars/10/719/635463755215710939.png",
+	"title":"Artillects",
+	"description":"Destroyer of Worlds. Mainly Artillects is setup as an anything-everything mod to prevent restricting what can be created. The foundation however is setup to focus on creating a wide range of non-player characters(NPC). These character are aimed at acting more or less like real living creatures. They will eat, sleep, talk, trade, work, and do what you normally will. For you as the player, or even tech player, these may not sound like much more than sims. However, the npcs are setup to allow for survival mode game play. You can trade with them for food, buy gear, and get them to do hard labor for you. Especially in the cases where you need to automate complex setups. For example reloading, and then aiming a missile at your buddies base with a payload of supplies. " //Short description for icon auto generated and derived from first 3 words of description.
 },
 {
-	"image":"img/logos/addictedtored.png",
-	"title":"Addicted To Red",
-	"description":"When you close your eyes at night, is the only thing you can think of Redstone? Good, you've come to the right place. We too have become addicted to the power of redstone and have made it even more powerful. Giving redstone all those detectors, manipulators, and cool machines it's always needed."
+	"image":"https://media-elerium.cursecdn.com/avatars/43/669/636024267118410364.jpeg",
+	"title":"Voltz Engine",
+	"description":"Voltz Engine is primarily a content creation system for mods created by the BBM Team. It provides basic tools for creating new items, tiles, machines, entity, ores, resources, world gen, and handling commands. The goal of Voltz Engine is to reduce the time taken to build and maintain mods. It does this by reducing the amount of code required by moving it to a data driven model." 
 },
 {
-	"image":"img/logos/aiimprovements.png",
+	"image":"https://media-elerium.cursecdn.com/avatars/29/625/635848662365584858.jpeg",
+	"title":"MFFS",
+	"description":"MFFS contains a series of machines focused on converting energy to Forton for the use in generating force fields. The primary machine is a force field projector which is fed by capacitors and generators. The projector can be modified with cards and upgrades in order to change logic. These upgrades include several field shapes, size scaling, position offset, and utility modules. For example, the upgrade to shock attacks, kill monsters, remove blocks, and protect tiles."
+},
+{
+	"image":"https://media-elerium.cursecdn.com/avatars/24/930/635765194096195753.png",
+	"title":"Armory",
+	"description":"Armory is a mod designed around the concept of providing everything a player needs to fight. This includes swords, hammers, maces, flintlocks, guns, rockets, grenades, and anything that can be held that kills. It also focuses on giving armor without creating an over power mess or imbalance to the game. While also focusing on giving a rich crafting and customization experience. "
+},
+{
+	"image":"https://media-elerium.cursecdn.com/avatars/38/848/635973269340770072.jpeg",
+	"title":"ICBM-Classic",
+	"description":"ICBM doesn't need much explanation of what it is or isn't. After all, ICBMs are super sizes missiles that blow everything up. Making this mod just about destroying the world right? I would say no but its a mixed story when it comes to ICBM. At the beginning, it was created with the idea that PvP needed a modern addition. Fewer swords, guns, traps, and more bang for your resources. Since its creation, it has evolved to support more than a PvP environment. Adding in cargo missile to transport items across the world. Weather missiles to control the climate and condition of the world. Transportation missiles to allow the player to take a one-way taxi back to his base. That is just the beginning of that is to come as the mod continues to evolve. "
+},
+{
+	"image":"https://media-elerium.cursecdn.com/avatars/128/40/636443912637541425.jpeg",
+	"title":"Come Closer",
+	"description":"Come closer is a simple mod that provides adjustments to name tag rendering to solve PvP issues. Mainly the problem of seeing players at long ranges and through walls."
+},
+{
+	"image":"https://media-elerium.cursecdn.com/avatars/87/397/636215345790175174.png",
+	"title":"Volts Classic",
+	"description":"Volts is a competitive lightweight PvP modpack focused around ICBM-Classic. It includes few mods so to allow people to focus more on competitive gameplay than on a technology race."
+},
+{
+	"image":"https://media-elerium.cursecdn.com/avatars/24/929/635765189952162048.png",
+	"title":"Assembly Line",
+	"description":"Assembly Line is a mod that focuses on large scale automation of factory-like environments. Its main feature is transportation & automation systems that integrate with a large series of machines. Its secondary feature is mass production type machines that can process items on a large scale without conventional pipe inputs.  "
+},
+{
+	"image":"https://media-elerium.cursecdn.com/avatars/21/699/635731173265581096.png",
 	"title":"AI Improvements",
-	"description":"If you have ever looked at a CPU usage report for Minecraft you will notice AI code at the top. This mod adds some changes to the AI code to decrease how much CPU the code uses."
+	"description":"This mod is focused on improving CPU clock times by reducing AI usage. While also allowing more control over AI functionality through simple customizations."
 },
 {
-	"image":"img/logos/bonetorch.png",
-	"title":"Bone Torch",
-	"description":"Light the halls with the bones of your enemies!"
+	"image":"https://media-elerium.cursecdn.com/avatars/26/454/635792154743798948.png",
+	"title":"Addicted To Red",
+	"description":"When you close your eyes at night all you can think about is Redstone? Good, you have come to the right place. We too have become addicted to the power of Redstone and have made it even more powerful. Giving Redstone all those detectors, manipulators, and cool machines it always needed."
 },
 {
-	"image":"img/logos/coloredchests.png",
-	"title":"Colored Chests",
-	"description":"Always hate it how you can't place double chests next to each other or figure out what chest is which. Colored chests adds a large variety of chests that support any RGB value."
+	"image":"https://media-elerium.cursecdn.com/avatars/83/625/636189659669504486.png",
+	"title":"Basic Industry",
+	"description":"Basic industry is the introduction mod in a series of technology mods planned by BBM. With that in mind, it provides the common functionality of other mods as well provides a new take on functionality. "
 },
 {
-	"image":"img/logos/redcow.png",
-	"title":"Red Cow",
-	"description":"Parody of Redbull energy drinks. This mod adds a potion item in the shape of a soda can that lets you fly. It even gives you wings on your back for the lolz of it."
+	"image":"https://media-elerium.cursecdn.com/avatars/39/325/635979606379845739.png",
+	"title":"Military Base Decor",
+	"description":"Military base decor is themed towards providing blocks and items to help style military bases in Minecraft."
 },
 {
-	"image":"img/logos/universalelectricity.png",
+	"image":"https://media-elerium.cursecdn.com/avatars/35/245/635921928368500118.png",
+	"title":"BBM Test Pack",
+	"description":"Lightweight sample Modpack to allow users to test the latest BBM mods."
+},
+{
+	"image":"https://media-elerium.cursecdn.com/avatars/42/953/636015571491779633.png",
+	"title":"Progressive World Cleanup",
+	"description":"Progress world cleanup is an automated utility for clearing out blocks and items in a world. It allows for admins to setup conditions to remove and replace blocks with little to no effort."
+},
+{
+	"image":"https://media-elerium.cursecdn.com/avatars/78/991/636176412207406358.png",
+	"title":"Volts 2.0",
+	"description":"Volts is a competitive light weight PvP modpack focused around ICBM and related BuiltBrokenModding mods. The pack is designed around fast paced technological combat with base buidling. This includes large missiles for blowing each other up and guns from the Armory mod themed after World War 1."
+},
+{
+	"image":"https://media-elerium.cursecdn.com/avatars/36/628/635940927179662788.jpeg",
+	"title":"Fallen Kingdoms",
+	"description":"Each kingdom falls in the wake of an unknown enemy. Only a few are left, can they rebuild their kingdoms in time?"
+},
+{
+	"image":"https://media-elerium.cursecdn.com/avatars/37/756/635956686785392383.png",
+	"title":"Ohms",
+	"description":"There use to be a time when wonder and adventure were king. A time before gears and transformers ruled the world. When monsters and magic were all that existed."
+},
+{
+	"image":"https://media-elerium.cursecdn.com/avatars/37/144/635948024970699904.png",
+	"title":"External Announcements",
+	"description":"External Announcements is a very basic modpack maker utility mod. That allows creating an external news file that will display in game. To provide updates about modpacks, server, or just advertise information."
+},
+{
+	"image":"https://media-elerium.cursecdn.com/avatars/24/676/635761058085944344.png",
 	"title":"Universal Electricity",
-	"description":"In the early game, it can be a pain to get a bucket for farming. This mod adds several wooden bucket types for storing any fluid without adding a new item id per fluid."
+	"description":"This is an archive of downloads for the now dead project Universal Electricity. No support will be offered for any bugs, or issues with the mod. Use at your own risk. "
 },
 {
-	"image":"img/logos/woodenbuckets.png",
-	"title":"Wooden Buckets",
-	"description":"In the early game, it can be a pain to get a bucket for farming. This mod adds several wooden bucket types for storing any fluid without adding a new item id per fluid."
+	"image":"https://media-elerium.cursecdn.com/avatars/4/271/635351425829399873.jpg",
+	"title":"Mad Science",
+	"description":"Our scientists have worked hard to bring you some of the finest equipment for genetic research Minecraft has to offer. These custom built machines fit into one of three separate categories. The categories represent the different stages of the game in which you will encounter them, and also will be expected to craft at that tier of the game."
 },
 {
-	"image":"img/logos/woodenrails.png",
-	"title":"Wooden Rails",
-	"description":"In the early game, it can be tricky getting into using minecarts. Especially with the high iron cost and all your machines needing that iron first. Wooden rails fixes this by adding wooden carts and tracks that are just a little slower than iron versions. Allowing for intergration of rail systems into your base without the pain of mining a lot of iron."
+	"image":"https://media-elerium.cursecdn.com/avatars/65/419/636162804509216428.png",
+	"title":"Staff Pack",
+	"description":"This is literally just a custom mod pack that is used for BBM's private staff server. Curse is being used to help easily distribute the pack between all members of BBM's staff and friends of the team. Anyone is welcome to install the back but it is basically an anything goes modpack. "
 }
+
 
 
 ];
