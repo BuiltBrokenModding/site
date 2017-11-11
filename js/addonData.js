9dc39f7102a00a50ec36c5fefbf882821ee57e02
Robert WC Seifert <rseifert.phone@gmail.com>
2017-11-11 11:26:46 -0500

    Adjusted mod lists, Added missing mods

--- js/addonData.js
+++ js/addonData.js
@@ -1,38 +1,11 @@
+//TODO cache icons on our site, curse will eventually get annoyed at the use of there media connection
+//TODO include links to curse mod author pages https://minecraft.curseforge.com/projects/icbm
+//TODO include images for scaled up version seperate from icons
 var addonDataArray = [
 {
-	"image":"https://media-elerium.cursecdn.com/avatars/10/719/635463755215710939.png",
-	"title":"Artillects",
-	"description":"Destroyer of Worlds. Mainly Artillects is setup as an anything-everything mod to prevent restricting what can be created. The foundation however is setup to focus on creating a wide range of non-player characters(NPC). These character are aimed at acting more or less like real living creatures. They will eat, sleep, talk, trade, work, and do what you normally will. For you as the player, or even tech player, these may not sound like much more than sims. However, the npcs are setup to allow for survival mode game play. You can trade with them for food, buy gear, and get them to do hard labor for you. Especially in the cases where you need to automate complex setups. For example reloading, and then aiming a missile at your buddies base with a payload of supplies. " //Short description for icon auto generated and derived from first 3 words of description.
-},
-{
-	"image":"https://media-elerium.cursecdn.com/avatars/43/669/636024267118410364.jpeg",
-	"title":"Voltz Engine",
-	"description":"Voltz Engine is primarily a content creation system for mods created by the BBM Team. It provides basic tools for creating new items, tiles, machines, entity, ores, resources, world gen, and handling commands. The goal of Voltz Engine is to reduce the time taken to build and maintain mods. It does this by reducing the amount of code required by moving it to a data driven model." 
-},
-{
-	"image":"https://media-elerium.cursecdn.com/avatars/29/625/635848662365584858.jpeg",
-	"title":"MFFS",
-	"description":"MFFS contains a series of machines focused on converting energy to Forton for the use in generating force fields. The primary machine is a force field projector which is fed by capacitors and generators. The projector can be modified with cards and upgrades in order to change logic. These upgrades include several field shapes, size scaling, position offset, and utility modules. For example, the upgrade to shock attacks, kill monsters, remove blocks, and protect tiles."
-},
-{
-	"image":"https://media-elerium.cursecdn.com/avatars/24/930/635765194096195753.png",
-	"title":"Armory",
-	"description":"Armory is a mod designed around the concept of providing everything a player needs to fight. This includes swords, hammers, maces, flintlocks, guns, rockets, grenades, and anything that can be held that kills. It also focuses on giving armor without creating an over power mess or imbalance to the game. While also focusing on giving a rich crafting and customization experience. "
-},
-{
-	"image":"https://media-elerium.cursecdn.com/avatars/38/848/635973269340770072.jpeg",
-	"title":"ICBM-Classic",
-	"description":"ICBM doesn't need much explanation of what it is or isn't. After all, ICBMs are super sizes missiles that blow everything up. Making this mod just about destroying the world right? I would say no but its a mixed story when it comes to ICBM. At the beginning, it was created with the idea that PvP needed a modern addition. Fewer swords, guns, traps, and more bang for your resources. Since its creation, it has evolved to support more than a PvP environment. Adding in cargo missile to transport items across the world. Weather missiles to control the climate and condition of the world. Transportation missiles to allow the player to take a one-way taxi back to his base. That is just the beginning of that is to come as the mod continues to evolve. "
-},
-{
-	"image":"https://media-elerium.cursecdn.com/avatars/128/40/636443912637541425.jpeg",
-	"title":"Come Closer",
-	"description":"Come closer is a simple mod that provides adjustments to name tag rendering to solve PvP issues. Mainly the problem of seeing players at long ranges and through walls."
-},
-{
-	"image":"https://media-elerium.cursecdn.com/avatars/87/397/636215345790175174.png",
-	"title":"Volts Classic",
-	"description":"Volts is a competitive lightweight PvP modpack focused around ICBM-Classic. It includes few mods so to allow people to focus more on competitive gameplay than on a technology race."
+	"image":"https://media-elerium.cursecdn.com/avatars/26/454/635792154743798948.png",
+	"title":"Addicted To Red",
+	"description":"When you close your eyes at night all you can think about is Redstone? Good, you have come to the right place. We too have become addicted to the power of Redstone and have made it even more powerful. Giving Redstone all those detectors, manipulators, and cool machines it always needed."
 },
 {
 	"image":"https://media-elerium.cursecdn.com/avatars/24/929/635765189952162048.png",
@@ -40,29 +13,86 @@ var addonDataArray = [
 	"description":"Assembly Line is a mod that focuses on large scale automation of factory-like environments. Its main feature is transportation & automation systems that integrate with a large series of machines. Its secondary feature is mass production type machines that can process items on a large scale without conventional pipe inputs.  "
 },
 {
-	"image":"https://media-elerium.cursecdn.com/avatars/21/699/635731173265581096.png",
-	"title":"AI Improvements",
-	"description":"This mod is focused on improving CPU clock times by reducing AI usage. While also allowing more control over AI functionality through simple customizations."
-},
-{
-	"image":"https://media-elerium.cursecdn.com/avatars/26/454/635792154743798948.png",
-	"title":"Addicted To Red",
-	"description":"When you close your eyes at night all you can think about is Redstone? Good, you have come to the right place. We too have become addicted to the power of Redstone and have made it even more powerful. Giving Redstone all those detectors, manipulators, and cool machines it always needed."
-},
-{
 	"image":"https://media-elerium.cursecdn.com/avatars/83/625/636189659669504486.png",
 	"title":"Basic Industry",
 	"description":"Basic industry is the introduction mod in a series of technology mods planned by BBM. With that in mind, it provides the common functionality of other mods as well provides a new take on functionality. "
 },
 {
+	"image":"https://media-elerium.cursecdn.com/avatars/128/40/636443912637541425.jpeg",
+	"title":"Come Closer",
+	"description":"Come closer is a simple mod that provides adjustments to name tag rendering to solve PvP issues. Mainly the problem of seeing players at long ranges and through walls."
+},
+{
+	"image":"https://media-elerium.cursecdn.com/avatars/79/111/636177022088785743.png",
+	"title":"ICBM",
+	"description":"ICBM doesn't need much explanation of what it is or isn't. After all, ICBMs are super sizes missiles that blow everything up. Making this mod just about destroying the world right? I would say no but its a mixed story when it comes to ICBM. At the beginning, it was created with the idea that PvP needed a modern addition. Fewer swords, guns, traps, and more bang for your resources. Since its creation, it has evolved to support more than a PvP environment. Adding in cargo missile to transport items across the world. Weather missiles to control the climate and condition of the world. Transportation missiles to allow the player to take a one-way taxi back to his base. That is just the beginning of that is to come as the mod continues to evolve. "
+},
+{
+	"image":"https://media-elerium.cursecdn.com/avatars/38/848/635973269340770072.jpeg",
+	"title":"ICBM-Classic",
+	"description":"When you want to kick back to a classic and blow up the world." //TODO need a unique description seperate from ICBM
+},
+{
+	"image":"https://media-elerium.cursecdn.com/avatars/29/625/635848662365584858.jpeg",
+	"title":"MFFS",
+	"description":"MFFS contains a series of machines focused on converting energy to Forton for the use in generating force fields. The primary machine is a force field projector which is fed by capacitors and generators. The projector can be modified with cards and upgrades in order to change logic. These upgrades include several field shapes, size scaling, position offset, and utility modules. For example, the upgrade to shock attacks, kill monsters, remove blocks, and protect tiles."
+},
+{
 	"image":"https://media-elerium.cursecdn.com/avatars/39/325/635979606379845739.png",
 	"title":"Military Base Decor",
 	"description":"Military base decor is themed towards providing blocks and items to help style military bases in Minecraft."
+}
+];
+
+var modpackData = [
+{
+	"image":"https://media-elerium.cursecdn.com/avatars/87/397/636215345790175174.png",
+	"title":"Volts Classic",
+	"description":"Volts is a competitive lightweight PvP modpack focused around ICBM-Classic. It includes few mods so to allow people to focus more on competitive gameplay than on a technology race."
 },
 {
-	"image":"https://media-elerium.cursecdn.com/avatars/35/245/635921928368500118.png",
-	"title":"BBM Test Pack",
-	"description":"Lightweight sample Modpack to allow users to test the latest BBM mods."
+	"image":"https://media-elerium.cursecdn.com/avatars/78/991/636176412207406358.png",
+	"title":"Volts II",
+	"description":"Volts is a competitive light weight PvP modpack focused around ICBM and related BuiltBrokenModding mods. The pack is designed around fast paced technological combat with base buidling. This includes large missiles for blowing each other up and guns from the Armory mod themed after World War 1."
+}
+];
+
+var libData = [
+{
+	"image":"https://media-elerium.cursecdn.com/avatars/43/669/636024267118410364.jpeg",
+	"title":"VoltzEngine",
+	"description":"Voltz Engine is primarily a content creation system for mods created by the BBM Team. It provides basic tools for creating new items, tiles, machines, entity, ores, resources, world gen, and handling commands. The goal of Voltz Engine is to reduce the time taken to build and maintain mods. It does this by reducing the amount of code required by moving it to a data driven model." 
+},
+{
+	"image":"https://media-elerium.cursecdn.com/avatars/91/863/636250605977785432.jpeg",
+	"title":"VE-Fluids",
+	"description":"VoltzEngine Fluid Module is an independent library from VoltzEngine to provide shared access to fluid prefabs. This includes a few fluids, fluid blocks, and fluid containers to be used. By itself, the mod provides a simple iron bucket that supports partial filling and all fluid types, unlike the vanilla bucket." //Short description for icon auto generated and derived from first 3 words of description.
+}
+];
+
+var armoryData = [
+{
+	"image":"https://media-elerium.cursecdn.com/avatars/24/930/635765194096195753.png",
+	"title":"Armory",
+	"description":"Armory is a mod designed around the concept of providing everything a player needs to fight. This includes swords, hammers, maces, flintlocks, guns, rockets, grenades, and anything that can be held that kills. It also focuses on giving armor without creating an over power mess or imbalance to the game. While also focusing on giving a rich crafting and customization experience. "
+},
+{
+	"image":"https://media-elerium.cursecdn.com/avatars/97/872/636295131722875078.png",
+	"title":"AA Sentries",
+	"description":"This mod adds Anti-Air and Anti-Missile automated sentry defense blocks using the Armory Mod." //Short description for icon auto generated and derived from first 3 words of description.
+},
+{
+	"image":"https://media-elerium.cursecdn.com/avatars/97/875/636295137884471581.png",
+	"title":"ICBM Sentries",
+	"description":"This mod adds 5 basic sentry guns from the original ICBM 1.6.4 mod. These sentry guns are built on the Armory Mod allowing compatible functionality with other sentries and full customization of content. This includes usage of the global access system in Voltz Engine and the entity target selection system in Armory."
+}
+];
+
+var extensionData = [
+{
+	"image":"https://media-elerium.cursecdn.com/avatars/37/144/635948024970699904.png",
+	"title":"External Announce",
+	"description":"External Announcements is a very basic modpack maker utility mod. That allows creating an external news file that will display in game. To provide updates about modpacks, server, or just advertise information."
 },
 {
 	"image":"https://media-elerium.cursecdn.com/avatars/42/953/636015571491779633.png",
@@ -70,54 +100,9 @@ var addonDataArray = [
 	"description":"Progress world cleanup is an automated utility for clearing out blocks and items in a world. It allows for admins to setup conditions to remove and replace blocks with little to no effort."
 },
 {
-	"image":"https://media-elerium.cursecdn.com/avatars/78/991/636176412207406358.png",
-	"title":"Volts 2.0",
-	"description":"Volts is a competitive light weight PvP modpack focused around ICBM and related BuiltBrokenModding mods. The pack is designed around fast paced technological combat with base buidling. This includes large missiles for blowing each other up and guns from the Armory mod themed after World War 1."
-},
-{
-	"image":"https://media-elerium.cursecdn.com/avatars/36/628/635940927179662788.jpeg",
-	"title":"Fallen Kingdoms",
-	"description":"Each kingdom falls in the wake of an unknown enemy. Only a few are left, can they rebuild their kingdoms in time?"
-},
-{
-	"image":"https://media-elerium.cursecdn.com/avatars/37/756/635956686785392383.png",
-	"title":"Ohms",
-	"description":"There use to be a time when wonder and adventure were king. A time before gears and transformers ruled the world. When monsters and magic were all that existed."
-},
-{
-	"image":"https://media-elerium.cursecdn.com/avatars/37/144/635948024970699904.png",
-	"title":"External Announce",
-	"description":"External Announcements is a very basic modpack maker utility mod. That allows creating an external news file that will display in game. To provide updates about modpacks, server, or just advertise information."
-},
-{
-	"image":"https://media-elerium.cursecdn.com/avatars/24/676/635761058085944344.png",
-	"title":"Universal Electricity",
-	"description":"This is an archive of downloads for the now dead project Universal Electricity. No support will be offered for any bugs, or issues with the mod. Use at your own risk. "
-},
-{
-	"image":"https://media-elerium.cursecdn.com/avatars/4/271/635351425829399873.jpg",
-	"title":"Mad Science",
-	"description":"Our scientists have worked hard to bring you some of the finest equipment for genetic research Minecraft has to offer. These custom built machines fit into one of three separate categories. The categories represent the different stages of the game in which you will encounter them, and also will be expected to craft at that tier of the game."
-},
-{
-	"image":"https://media-elerium.cursecdn.com/avatars/65/419/636162804509216428.png",
-	"title":"Staff Pack",
-	"description":"This is literally just a custom mod pack that is used for BBM's private staff server. Curse is being used to help easily distribute the pack between all members of BBM's staff and friends of the team. Anyone is welcome to install the back but it is basically an anything goes modpack. "
-}
-
-
-
-];
-
-var extensionData = [
-{
-	"image":"https://media-elerium.cursecdn.com/avatars/91/863/636250605977785432.jpeg",
-	"title":"VE-FluidHandling",
-	"description":"VoltzEngine Fluid Module is an independent library from VoltzEngine to provide shared access to fluid prefabs. This includes a few fluids, fluid blocks, and fluid containers to be used. By itself, the mod provides a simple iron bucket that supports partial filling and all fluid types, unlike the vanilla bucket." //Short description for icon auto generated and derived from first 3 words of description.
-},{
-	"image":"https://media-elerium.cursecdn.com/avatars/97/872/636295131722875078.png",
-	"title":"[Armory] Sentry AA & AMS",
-	"description":"This mod adds Anti-Air and Anti-Missile automated sentry defense blocks using the Armory Mod." //Short description for icon auto generated and derived from first 3 words of description.
+	"image":"https://media-elerium.cursecdn.com/avatars/21/699/635731173265581096.png",
+	"title":"AI Improvements",
+	"description":"This mod is focused on improving CPU clock times by reducing AI usage. While also allowing more control over AI functionality through simple customizations."
 }
 ];
 
