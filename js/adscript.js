8385a4d8a097f6688b3d6e267e0277ee0a13ad8e
Robert WC Seifert <rseifert.phone@gmail.com>
2017-12-02 11:45:51 -0500

    Updated ads

--- js/adscript.js
+++ js/adscript.js
@@ -1,5 +1,4 @@
-var darksScript = '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><!-- BBM Large Ad --><ins class="adsbygoogle"     style="display:inline-block;width:300px;height:600px"    data-ad-client="ca-pub-2991607480093590"     data-ad-slot="8788848951"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>';
+var adScript = '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><!-- BBM Ad --><ins class="adsbygoogle" style="display:inline-block;width:300px;height:600px" data-ad-client="ca-pub-2991607480093590" data-ad-slot="7085263310"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>';
 
-
-document.getElementById('leftAd').innerHTML = darksScript;
-document.getElementById('rightAd').innerHTML = darksScript;
+document.getElementById('leftAd').innerHTML = adScript;
+document.getElementById('rightAd').innerHTML = adScript;
