29f5a4bb8a2197047bc97957668aee0b2a47a912
Samuel Walker <sfxworks@gmail.com>
2017-12-25 10:35:05 -0500

    Fixed news
    Fixes an issue where 2016's news wouldnt show

--- js/news/newsscript.js
+++ js/news/newsscript.js
@@ -1,45 +1,35 @@
-    var newsObject = {};
+var newsObject = {};		
 
-	var news = JSON.stringify(j2017);
-	news = JSON.parse(news);
-    newsObject.twentyseventeen = news;
-        
-	for (var i=0; i < news.length; i++)
-	{
-		var str = news[i].title;
-		var titleHash = str.replace(/\s+/g, '');
-		document.getElementById("twentyseventeenlist").innerHTML += 
-            '<li class="list-group-item"><a onClick="onNewsClick(' + i + ', \'twentyseventeen\')" href="#' + titleHash + '"><h6>' + news[i].title +'</h6><span>' + news[i].date + ' - ' + news[i].author + '</span></a></li>'
-	}
-    
-    news = JSON.stringify(j2016);
-    news = JSON.parse(news);
-    
-    newsObject.twentysixteen = news;
-        
-    
-    for (var i=0; i < news.length; i++)
-	{
-		var str = news[i].title;
-		var titleHash = str.replace(/\s+/g, '');
-		document.getElementById("twentysixteenlist").innerHTMeL += 
-            '<li class="list-group-item"><a onClick="onNewsClick(' + i + ', \'twentysixteen\' )" href="#' + titleHash + '"><h6>' + news[i].title +'</h6><span>' + news[i].date + ' - ' + news[i].author + '</span></a></li>'
-	}
-    
-	function onNewsClick(aN, year) //Article Number
-	{
-		document.getElementById("reportTitle").innerHTML = newsObject[year][aN].title;
-		document.getElementById("reportContent").innerHTML = newsObject[year][aN].content;
-		/*
-		if(news[aN].content.length > 100)
-		{
-			document.getElementById("reportContent").style.overflowy = "scroll";
-		}
-		else
-		{
-			document.getElementById("reportContent").style.overflowy = "hidden";
-		}*/
-	}
-	//Load latest article by default.
-	document.getElementById("reportTitle").innerHTML = newsObject.twentyseventeen[1].title;
-	document.getElementById("reportContent").innerHTML = newsObject.twentyseventeen[1].content;
+var news = JSON.stringify(j2017);		
+news = JSON.parse(news);		
+newsObject.twentyseventeen = news;		
+
+for (var i=0; i < news.length; i++)		
+{		
+    var str = news[i].title;		
+    var titleHash = str.replace(/\s+/g, '');		
+    document.getElementById("twentyseventeenlist").innerHTML += 		
+         '<li class="list-group-item"><a onClick="onNewsClick(' + i + ', \'twentyseventeen\')" href="#' + titleHash + '"><h6>' + news[i].title +'</h6><span>' + news[i].date + ' - ' + news[i].author + '</span></a></li>';	
+}		
+
+news = JSON.stringify(j2016);		
+news = JSON.parse(news);		
+
+newsObject.twentysixteen = news;		
+
+for (var i=0; i < news.length; i++)		
+{		
+    var str = news[i].title;		
+    var titleHash = str.replace(/\s+/g, '');		
+    document.getElementById("twentysixteenlist").innerHTML += 		
+         '<li class="list-group-item"><a onClick="onNewsClick(' + i + ', \'twentysixteen\' )" href="#' + titleHash + '"><h6>' + news[i].title +'</h6><span>' + news[i].date + ' - ' + news[i].author + '</span></a></li>';
+}		
+
+function onNewsClick(aN, year) //Article Number		
+{		
+    document.getElementById("reportTitle").innerHTML = newsObject[year][aN].title;		
+    document.getElementById("reportContent").innerHTML = newsObject[year][aN].content;			
+}		
+//Load latest article by default.		
+document.getElementById("reportTitle").innerHTML = newsObject.twentyseventeen[1].title;		
+document.getElementById("reportContent").innerHTML = newsObject.twentyseventeen[1].content;
