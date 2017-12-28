b89d9225c117e11df276fbba093f667082fcb595
Samuel Walker <sfxworks@gmail.com>
2017-12-28 05:22:29 -0500

    Popup bar
    Partially resolves #30 in that it only shows the pop up every 10 miuntes. It does not recreate the pop up if the user idles. Only checks to see if it's been 10 minutes since the user last visited.

--- js/announcements.js
+++ js/announcements.js
@@ -10,21 +10,70 @@ announcements.push("Test Announcement 3");
 
 
 //Announcement Logic
-var selectedAnnouncement = Math.floor(Math.random() * (announcements.length));
 
-document.getElementById("randomText").innerHTML = announcements[selectedAnnouncement];
+var announcementShown = getCookie("announcementShown")
+console.log(announcementShown);
 
-var showAdOrNah = Math.round(Math.random());
-if(showAdOrNah > 0)
+if(announcementShown == "")
 {
-    document.getElementById("footer1").innerHTML = "";
+    showAnnouncement();
 }
 else
 {
+    document.getElementById("footer1").innerHTML = "";
     document.getElementById("footer2").innerHTML = "";
+}
+
+
+function showAnnouncement()
+{
+    var d = new Date();
+    d.setTime(d.getTime() + (10 * 60 * 1000));
+    setCookie("announcementShown", d.toUTCString());
+    
+    var selectedAnnouncement = Math.floor(Math.random() * (announcements.length));
+
+    document.getElementById("randomText").innerHTML = announcements[selectedAnnouncement];
+
+    var showAdOrNah = Math.round(Math.random());
+    if(showAdOrNah > 0)
+    {
+        document.getElementById("footer1").innerHTML = "";
+    }
+    else
+    {
+        document.getElementById("footer2").innerHTML = "";
+    }
+
+
 }
 
 function destroyFooter()
 {
     document.getElementById("footer1").innerHTML = "";
+}
+
+function getCookie(cname)
+{
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function setCookie(cname, cvalue)
+{
+    var d = new Date();
+    d.setTime(d.getTime() + (10 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = "announcementShown=" + true + ";" + expires + ";path=/";
 }
