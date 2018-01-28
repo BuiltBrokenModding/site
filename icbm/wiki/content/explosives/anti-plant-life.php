ad5c0a710888439ccd885afd4a497b9e1e81c273
Robert WC Seifert <rseifert.phone@gmail.com>
2018-01-27 23:59:08 -0500

    Restored old wiki, will fix broken php later

---
+++ icbm/wiki/content/explosives/anti-plant-life.php
@@ -0,0 +1,57 @@
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/icbm/wiki/wiki.top.php"); ?>
+<script>
+var embeds = ['https://www.youtube.com/embed/jzNcHmGJu6g?rel=0&amp;controls=0', 'https://www.youtube.com/embed/DayXQuoDoBQ?rel=0&amp;controls=0'];
+</script>
+
+<link rel="stylesheet" type="text/css" href="../../wikistyle.css">
+<h1> Warhead - Anti-Plant Life Fit </h1>
+<h2  id="warhead"> </h2>
+<br/>
+<table>
+<tr>
+    <td class="arrowleft">
+        <input id="arrowleft" type="image" src="../../../../../img/icbm/buranku.png" width="70" height="315"/>
+    </td>
+    <td class="video">
+        <iframe id="iframe" width="420" height="315" frameborder="0" allowfullscreen></iframe>
+    </td>
+    <td class="content">
+    <div id="1" style="display:none">
+        <p>Is your peaceful wishful-thinking neighbor trying to earn a living by growing crops in his back yard? Destroy them...with The Anti Plant Life fit! Add this to a Micro Warhead and watch those long awaited crops within a two block radius of the target disappear into thin air. Thin, non-appetizing air.</p>
+        <br/>
+        <h2>Recipe</h2>
+        <img id="recimg" src="../../../../../img/icbm/recipe-microwarhead-antiplantlife.png" class="recipeimage" src="" width="108" height="108" alt="Micro Warhead - Anti-Plant Life Fit recipe"/>
+        <br/>	
+        <p class="recipedescription">Leaves. Spider Eyes. Blaze Powder. Burn the spidereyes in the leaves with fire. Self explanatory, right? Oh, and the microwarhead ofcourse.
+        
+        </p>
+        <br/>
+    </div>
+    <div id="2" style="display:none">
+        <p>The small warhead had a radius of five. This will lead to massive devastation of your friends backyard. Or maybe...those villagers over in the distance that seem to be stealing your farm's revenue?</p>
+        <br/>
+        <h2>Recipe</h2>
+        <img id="recimg" src="../../../../../img/icbm/recipe-smallwarhead-antiplantlife.png" class="recipeimage" src="" width="108" height="108" alt="Small Warhead - Anti-Plant Life Fit recipe"/>
+        <br/>
+        <p class="recipedescription">Moar Leaves. Moar Spider Eyes. Moar Blaze Powder. Burn moar of the spider eyes in moar leaves with moar fire. Self explanatory, right?
+        
+        </p>
+        <br/>
+    </div>
+    </td>
+    <td class="arrowright">
+        <input id="arrowright" type="image" src="../../../../../img/icbm/buranku.png" width="70" height="315"/>
+    </td>
+</tr>
+</table>
+<br/>
+<div class="history">
+    <h2>History</h2>
+    <br/>
+    <p>Previewed on 12/10/2015 on <a href="https://www.youtube.com/watch?v=SfYcF8fqjl0">DarkGuardsman's channel</a>.</p>
+    <br/>
+</div>
+    <script src="../../../../../js/wikicontentchanger.js"></script>
+    
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/icbm/wiki/content.php"); ?>
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/php/bottom.php"); ?>
