72d5f452c81a9d88d4b0ad560feb73448808b863
Samuel Walker <services@sfxworks.net>
2018-09-21 00:51:21 -0400

    News get and post
    News get and post mechanics. Basic template. Requires auth header for posting. Getting renders its own page using pug.

--- server.js
+++ server.js
@@ -24,7 +24,6 @@ var Project = mongoose.model('Project', {
   type: String, //Lib, Modpack, Mod, Utility
   authors: Array,
   releaseDate: Date,
-  tags: Array
   tags: Array,
   description: String,
   subText: String,
@@ -68,7 +67,25 @@ app.get('/project/:name', (req, res) =>{
     if(project == null)
       return res.sendStatus(404)
     
-    res.render('project-tempalte', {cache: true, title: 'Built Broken Studios - Projects', description})
+    res.render('project-tempalte', {cache: true, title: project.name, description: project.subText, img: project.image})
+  })
+})
+
+//News can now be rendered on a page by page basis further increasing SEO
+app.get('/news/:articleTitle', (req, res) =>{
+  console.log('User requesting news article ' + req.params.articleTitle)
+  let titleTransform = req.params.articleTitle.replace(/-/g,' ') //Because hiphens are better than %20
+  console.log('Title transformed to ' + titleTransform)
+  News.findOne({title: titleTransform}, (err, news) => {
+    if(err)
+      return res.sendStatus(500)
+    if(news == null)
+      return res.sendStatus(400)
+
+    console.log('Got')
+    console.log(news.body)
+    
+    res.render('news', {cache: false, title: news.title, description: news.subText, img: news.image, content: news.body, date: news.date, author: news.author})
   })
 })
 
@@ -78,14 +95,57 @@ app.get('/', (req,res) =>{
 })
 
 app.get('/project', (req, res) =>{
-  return res.render('project-overview', {cache: true, title: 'Built Broken Studios - Projects', description: 'Body Test',  img: '/img/mods/assemblyline.png', } )
+  return res.render('project-overview', {cache: true, title: 'Built Broken Studios - Projects', description: 'We have a lot of projects. Mods, libiraries and others included!',  img: '/img/mods/assemblyline.png', } )
 })
 
 app.get('/about', (req, res) =>{
-  return res.render('about', {cache: true, title: 'Built Broken Studios - Projects', description: 'Body Test',  img: '/img/mods/assemblyline.png'})
+  return res.render('about', {cache: true, title: 'Built Broken Studios - Projects', description: 'Built broken studios is a crew of awesome developers.',  img: '/img/mods/assemblyline.png'})
 })
 
-app.get('/content')
+app.get('/contact', (req, res) =>{
+  return res.render('contact', {cache: true, title: 'Built Broken Studios - Contact', description: 'Discord, Twitter, Github and other points of contact!',  img: '/img/Gear-icon.png'})
+})
+
+//Posting for news
+app.post('/news', (req, res) =>{
+  //State missing info / Deny without auth header
+  let authHeader = req.get('X-AUTH-HEADER')
+  let contentType = req.get('Content-Type')
+  if(!authHeader)
+    return res.sendStatus(401)
+
+  //Need content type specified to parse.
+  if(contentType != 'application/json')
+    return res.sendStatus(400)
+
+  //Deny if auth missing
+  if(server_config.AUTH_HEADER != req.get('X-AUTH-HEADER'))
+    return res.sendStatus(401)
+
+  if(req.get)
+  /*
+  title: String,
+  subText: String,
+  image: String,
+  date: Date,
+  author: String,
+  body: String
+  */
+
+  //This is only for DB error posting. Express will return a separate error if json is malformed.
+  //EX: SyntaxError: Unexpected token &#39; in JSON at position 172
+  var news = new News(req.body)
+  news.save((err) => {
+    if(err)
+      return res.sendStatus(500)
+    
+    return res.sendStatus(201)
+    
+  })
+  
+})
+
+//Open post paths
 
 //Expose server
 var server = http.listen(8080, () => {
